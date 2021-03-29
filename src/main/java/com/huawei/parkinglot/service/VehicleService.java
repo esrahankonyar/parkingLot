0958aeae2d8b6ef452c04932d4ba9acb7d0677b3
esrahankonyar <konyar.esrahan@gmail.com>
2021-03-29 23:57:12 +0300

    Refactor: diffHours method added for other classes implementing vehicleService

--- src/main/java/com/huawei/parkinglot/service/VehicleService.java
+++ src/main/java/com/huawei/parkinglot/service/VehicleService.java
@@ -4,10 +4,9 @@ import com.huawei.parkinglot.entity.CheckInOut;
 import com.huawei.parkinglot.entity.vehicle.Vehicle;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
-/**
- * Calculations will be placed on each vehicle type
- */
+
 public interface VehicleService {
 
 	void checkIn(CheckInOut checkInOut);
@@ -15,6 +14,14 @@ public interface VehicleService {
 	void checkOut(Vehicle vehicle);
 
 	List<CheckInOut> getParkingDetails(String licensePlate);
+
+	default Double diffHours(Long checkInTime, Long checkOutTime){
+
+		Long diffMinutes = checkInTime - checkOutTime;
+		diffMinutes = TimeUnit.MILLISECONDS.toMinutes(diffMinutes);
+		return  (double) diffMinutes / 60;
+
+	}
 
 
 }
